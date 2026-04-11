bf9b9dae16c54612fa67dd4a3527922a27fd4144
QinBu_Rua <qmr_1104@163.com>
2026-04-11 11:55:36 +0800

    新增(EpubResult): 新增构造函数，使用string或Exception构造EpubResult

--- TxtToEpubMaker/Structs/EpubResult.cs
+++ TxtToEpubMaker/Structs/EpubResult.cs
@@ -1,10 +1,38 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace TxtToEpubMaker.Structs;
 
 public struct EpubResult
 {
-    public string? EpubPath { get; set; }
-    public required bool Success { get; set; }
-    public string? ErrorMessage { get; set; }
+    public string? EpubPath { get; init; }
+    public required bool Success { get; init; }
+    public string? ErrorMessage { get; init; }
+
+    [JsonIgnore] public Exception? InnerException { get; init; }
+
+    [SetsRequiredMembers]
+    public EpubResult(string epubPath)
+    {
+        EpubPath = epubPath;
+        Success = true;
+        ErrorMessage = null;
+        InnerException = null;
+    }
+
+    [SetsRequiredMembers]
+    public EpubResult(Exception exception)
+    {
+        EpubPath = null;
+        Success = false;
+        InnerException = exception;
+
+        ErrorMessage = InnerException switch
+        {
+            JsonException jsonException 
+                => $"{jsonException.GetType().FullName}: {jsonException.Message} | Line: {jsonException.LineNumber}",
+            _ => $"{InnerException.GetType().FullName}: {InnerException.Message}"
+        };
+    }
 }
